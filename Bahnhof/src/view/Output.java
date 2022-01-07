296a71d909d6cbe2b8f8c1067be76d2046823580
Jordan-Velte <jordan.velte@gymhum.de>
2022-01-07 08:49:14 +0100

    mvc

---
+++ Bahnhof/src/view/Output.java
@@ -0,0 +1,9 @@
+package view;
+
+public class Output {
+    //Ausgabemethode: Durch Parameterübergabe werden übergebene Werte als String (data) ausgegeben --> View
+    public void printData(String data){
+        System.out.println(data);
+    }
+}
+
